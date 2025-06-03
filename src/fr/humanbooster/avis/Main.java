ad03fddee65cb8d0f2764e8176dd3fcebac55281
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-06-03 13:47:08 +0200

    Fix: re-added equal() & hashCode()

--- src/fr/humanbooster/avis/Main.java
+++ src/fr/humanbooster/avis/Main.java
@@ -12,9 +12,9 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Utilisateur> utilisateurs = new ArrayList<>();
 
-        Moderateur moderateur = new Moderateur("Sam", "", "sam@admin.test", "");
-        Joueur joueur1 = new Joueur("Matt", "","", LocalDate.of(1970, 1, 1));
-        Joueur joueur2 = new Joueur("Matt", "","", LocalDate.of(1970, 1, 1));
+        Moderateur moderateur = new Moderateur("Sam", "p@s$w0rd", "sam@admin.test", "0600000000");
+        Joueur joueur1 = new Joueur("Matt", "p@s$w0rd","matt@user.test", LocalDate.of(1970, 1, 1));
+        Joueur joueur2 = new Joueur("Matt", "p@s$w0rd","matt@user.test", LocalDate.of(1970, 1, 1));
 
         Collections.addAll(utilisateurs, moderateur, joueur1, joueur2);
 
@@ -24,9 +24,9 @@ public class Main {
             // Vérifier la classe de l'objet & l'instancier + caster dans une variable pour l'utiliser dans l'IF
             // (pratique si on doit l'utiliser plusieurs fois)
             if (utilisateur instanceof  Moderateur mod) {
-                System.out.println("(Moderateur) " +  mod);
-                System.out.println("--Tél: " + mod.getNumeroDeTelephone());
-                // Vérifier la classe de l'objet et - une fois dans l'IF — le caster pour l'utiliser
+                System.out.println("(Modérateur) " +  mod);
+                System.out.println("-- Tél: " + mod.getNumeroDeTelephone());
+                // Vérifier la classe de l'objet et (une fois dans l'IF) le caster pour l'utiliser
                 // (pratique si on doit l'utiliser une seule fois)
             } else if(utilisateur instanceof Joueur) {
                 System.out.println("(Joueur) " +  utilisateur);
@@ -34,6 +34,10 @@ public class Main {
             } else {
                 System.out.println("(Utilisateur) " +  utilisateur);
             }
+
+            System.out.println("--- Equals (joueur1 & joueur2): " + joueur1.equals(joueur2));
+            System.out.println("--- HashCode Joueur1: " + joueur1.hashCode());
+            System.out.println("--- HashCode Joueur2: " + joueur2.hashCode());
         }
     }
 }
